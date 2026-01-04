ea63b60b6e865e949d5d7e1aa51ef086df4fa1fe
V Arvind <varvind@chimeratechnologies.com>
2026-01-04 21:01:06 +0530

    Add missing Color import

--- app/src/main/java/com/expensetracker/presentation/classify/ClassifyScreen.kt
+++ app/src/main/java/com/expensetracker/presentation/classify/ClassifyScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.boundsInRoot
